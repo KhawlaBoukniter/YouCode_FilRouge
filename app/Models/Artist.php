8e04c85b9bc404cf5cfe82b57cb85ef9e0c7514f
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-18 14:00:00 +0000

    added Eloquent relations

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artist extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function artworks()
+    {
+        return $this->hasMany(Artwork::class);
+    }
 }
--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artwork extends Model
 {
-    //
+    public function artist()
+    {
+        return $this->belongsTo(Artist::class);
+    }
 }
