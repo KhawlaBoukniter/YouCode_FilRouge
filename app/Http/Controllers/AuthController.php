d7ffeed7d9f4a3444b2792073c26d6f7e570917e
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-18 10:32:11 +0000

    implement user registration with default role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role_id' => Role::where('name', 'visitor')->first()->id,
+        ]);
+
+        return response()->json(['message' => 'user registred'], 201);
+    }
 }
