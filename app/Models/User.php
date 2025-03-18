02e76dd9aca5f7196cab484db121a72db2cdea75
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-18 09:34:31 +0000

    seed default roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function role() {
-        return $this->belongsTo(Role:class);
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
     }
 }
