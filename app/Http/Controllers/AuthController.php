ef36a039eb3a4923993e6388861b61deeb0141ff
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-18 11:24:20 +0000

    implement user login with passport

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -30,5 +31,26 @@ class AuthController extends Controller
         ]);
 
         return response()->json(['message' => 'user registred'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'string|required|email',
+            'password' => 'string|required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('authToken')->accessToken;
+
+        return response()->json(['token' => $token, 'user' => $user], 200);
     }
 }
