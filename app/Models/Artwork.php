033bb6a22bbbd43e684cd2a4252a5309bebbc130
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-24 00:05:00 +0000

    � add likes() relation to Artwork model

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -22,4 +22,9 @@ class Artwork extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function likes()
+    {
+        return $this->belongsToMany(User::class, 'likes')->withTimestamps();
+    }
 }
