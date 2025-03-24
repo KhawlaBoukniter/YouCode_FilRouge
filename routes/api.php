25cb80b99e9b82ff5e8e580cff122afb6717f812
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-24 20:00:00 +0000

    feat: register DELETE /reservations/{reservation} route

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,7 @@ Route::middleware('auth:api')->group(function () {
         Route::get('/my-reservations', [ReservationController::class, 'index']);
         Route::put('/reservations/{reservation}/pay', [ReservationController::class, 'pay']);
         Route::put('/reservations/{reservation}/cancel', [ReservationController::class, 'cancel']);
+        Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy']);
     });
 
     Route::get('/artworks', [ArtworkController::class, 'index']);
