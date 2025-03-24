882bb3023f2fad1d5b232decf2454d1a9431bd18
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-24 12:42:00 +0000

    � add Ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     protected $fillable = [
-        'user_id',
         'event_id',
         'quantity',
+        'price',
+        'type',
+        'description',
         'status',
     ];
 
