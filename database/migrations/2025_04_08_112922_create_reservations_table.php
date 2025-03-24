d3c676159c71685ea80a13aef31f749baf7919df
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-24 12:04:00 +0000

    � create reservations table to store ticket reservations by visitors

---
+++ database/migrations/2025_04_08_112922_create_reservations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('ticket_id')->constrained()->onDelete('cascade');
+            $table->unsignedInteger('quantity')->default(0);
+            $table->enum('status', ['reserved', 'paid', 'cancelled'])->default('reserved');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
