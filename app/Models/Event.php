e74659e40184662225010f91b1be4702b691ad6d
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-24 03:45:00 +0000

    � add Event model with fillable and tickets() relation

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Ticket;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'location',
+        'is_online',
+        'poster',
+    ];
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
