d3fc9891266a42c9a0ee887b166c8c5c4ac8d1a6
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-24 19:21:00 +0000

    refactor: update ticket status default value to 'available' in tickets table migration

---
+++ database/migrations/2025_04_09_151451_alter_status_column_in_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->enum('status', ['available', 'paid', 'cancelled'])->default('available');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->enum('status', ['reserved', 'paid', 'cancelled'])->default('reserved');
+        });
+    }
+};
