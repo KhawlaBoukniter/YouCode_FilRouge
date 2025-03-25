4fea4aa30afd479dffbf4ec6b17287fb19286e48
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-25 09:27:00 +0000

    feat: add /museum route to display 3D scene

--- artspace-frontend/src/App.js
+++ artspace-frontend/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import RevolutionaryFeatures from "./components/RevolutionaryFeatures";
-import FeaturedExhibitions from "./components/FeaturedExhibitions";
-import Testimonials from "./components/Testimonials";
-import CallToAction from "./components/CallToAction";
-import Footer from "./components/Footer";
+// import HomePage from './pages/HomePage';
+// import Navbar from "./components/Navbar";
+// import HeroSection from "./components/HeroSection";
+// import RevolutionaryFeatures from "./components/RevolutionaryFeatures";
+// import FeaturedExhibitions from "./components/FeaturedExhibitions";
+// import Testimonials from "./components/Testimonials";
+// import CallToAction from "./components/CallToAction";
+// import Footer from "./components/Footer";
+import Museum3D from './pages/3D/Museum3D'
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <HeroSection />
+//       <RevolutionaryFeatures />
+//       <FeaturedExhibitions />
+//       <Testimonials />
+//       <CallToAction />
+//       <Footer />
+//     </>
+//   );
+// }
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <HeroSection />
-      <RevolutionaryFeatures />
-      <FeaturedExhibitions />
-      <Testimonials />
-      <CallToAction />
-      <Footer />
-    </>
-  );
+    <Routes>
+      <Route path="/museum" element={<Museum3D />} />
+    </Routes>
+  )
 }
 
 export default App;
