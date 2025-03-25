64fe27f99f2c786e64097b2a072387c558d90f7f
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-25 22:43:00 +0000

    feat: add methods in StatsRepository for tickets sold, revenue, and top events

---
+++ app/Repositories/StatsRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Reservation;
+use Illuminate\Support\Facades\DB;
+
+class StatsRepository
+{
+    public function getTotalTicketsSold($artistId)
+    {
+        return Reservation::whereHas('ticket.event', function ($query) use ($artistId) {
+            $query->where('artist_id', $artistId);
+        })->sum('quantity');
+    }
+
+    public function getTotalRevenue($artistId)
+    {
+        return Reservation::whereHas('ticket.event', function ($query) use ($artistId) {
+            $query->where('artist_id', $artistId);
+        })->where('status', 'paid')->where('tickets', 'reservations.ticket_id', '=', 'tickets.id')
+            ->sum(DB::raw('tickets.price * reservations.quantity'));
+    }
+
+    public function getTopEvents($artistId)
+    {
+        return Reservation::select('tickets.event_id', DB::raw('SUM(reservations.quantity) as totalsold'))
+            ->join('tickets', 'reservations.ticket_id', '=', 'tickets.id')
+            ->whereHas('ticket.event', function ($query) use ($artistId) {
+                $query->where('artist_id', $artistId);
+            })->groupBy('tickets.event_id')->orderByDesc('total_sold')->take(5)->get();
+    }
+}
