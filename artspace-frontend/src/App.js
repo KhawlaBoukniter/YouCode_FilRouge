53fbf037ef52d4f0d77b6c77f4e2eb337b938246
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-25 17:55:00 +0000

    feat: set up dynamic routing for museum rooms and homepage

--- artspace-frontend/src/App.js
+++ artspace-frontend/src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import HomePage from './pages/HomePage';
-// import Navbar from "./components/Navbar";
-// import HeroSection from "./components/HeroSection";
-// import RevolutionaryFeatures from "./components/RevolutionaryFeatures";
-// import FeaturedExhibitions from "./components/FeaturedExhibitions";
-// import Testimonials from "./components/Testimonials";
-// import CallToAction from "./components/CallToAction";
-// import Footer from "./components/Footer";
-import Museum3D from './pages/3D/Museum3D'
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <HeroSection />
-//       <RevolutionaryFeatures />
-//       <FeaturedExhibitions />
-//       <Testimonials />
-//       <CallToAction />
-//       <Footer />
-//     </>
-//   );
-// }
+import HomePage from './pages/HomePage';
+import Museum3D from './pages/3D/Museum3D';
 
 function App() {
   return (
     <Routes>
-      <Route path="/museum" element={<Museum3D />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/museum/:roomId" element={<Museum3D />} />
     </Routes>
-  )
+  );
 }
 
 export default App;
