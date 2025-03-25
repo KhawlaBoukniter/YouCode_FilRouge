96b9f60184a696637b6bb540037bd20ad5d14b23
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-25 18:02:00 +0000

    feat: enable dynamic 3D room rendering based on route with menu and ClickToMove

--- artspace-frontend/src/pages/3D/Museum3D.js
+++ artspace-frontend/src/pages/3D/Museum3D.js
@@ -1,16 +1,18 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { useParams } from 'react-router-dom'
+
 import ClickToMove from '../../components/3D/ClickToMove'
-import ClickableDoorZone from '../../components/3D/ClickableDoorZone'
+import RoomStyle1 from '../../components/3D/RoomStyle1'
+import RoomStyle2 from '../../components/3D/RoomStyle2'
+import RoomStyle3 from '../../components/3D/RoomStyle3'
+import RoomStyle4 from '../../components/3D/RoomStyle4'
+import RoomTD from '../../components/3D/RoomStyleTrending'
+import MuseumMenu from '../../components/3D/MuseumMenu'
 
 function Controls({ controlsRef }) {
     const { camera } = useThree()
-
-    useEffect(() => {
-        controlsRef.current.target.set(0, 2.5, 0)
-        controlsRef.current.update()
-    }, [])
 
     return (
         <OrbitControls
@@ -27,108 +29,32 @@ function Controls({ controlsRef }) {
     )
 }
 
-function Room({ position = [0, 0, 0], color = "#ffffff", floorColor = "#e0e0e0", withLeftDoor = false, withRightDoor = false, floorRef = null }) {
-    return (
-        <group position={position}>
-            {/* Sol */}
-            <mesh
-                ref={floorRef}
-                rotation={[-Math.PI / 2, 0, 0]}
-                receiveShadow
-            >
-                <planeGeometry args={[20, 20]} />
-                <meshStandardMaterial color={floorColor} />
-            </mesh>
-
-            {/* Mur arrière */}
-            <mesh position={[0, 2.5, -10]}>
-                <boxGeometry args={[20, 5, 0.2]} />
-                <meshStandardMaterial color={color} />
-            </mesh>
-
-            {/* Mur avant */}
-            <mesh position={[0, 2.5, 10]}>
-                <boxGeometry args={[20, 5, 0.2]} />
-                <meshStandardMaterial color={color} />
-            </mesh>
-
-            {/* Mur gauche */}
-            {withLeftDoor ? (
-                <>
-                    <mesh position={[-10, 2.5, -6.5]}>
-                        <boxGeometry args={[0.2, 5, 7]} />
-                        <meshStandardMaterial color={color} />
-                    </mesh>
-                    <mesh position={[-10, 2.5, 6.5]}>
-                        <boxGeometry args={[0.2, 5, 7]} />
-                        <meshStandardMaterial color={color} />
-                    </mesh>
-                </>
-            ) : (
-                <mesh position={[-10, 2.5, 0]}>
-                    <boxGeometry args={[0.2, 5, 20]} />
-                    <meshStandardMaterial color={color} />
-                </mesh>
-            )}
-
-            {/* Mur droit */}
-            {withRightDoor ? (
-                <>
-                    <mesh position={[10, 2.5, -6.5]}>
-                        <boxGeometry args={[0.2, 5, 7]} />
-                        <meshStandardMaterial color={color} />
-                    </mesh>
-                    <mesh position={[10, 2.5, 6.5]}>
-                        <boxGeometry args={[0.2, 5, 7]} />
-                        <meshStandardMaterial color={color} />
-                    </mesh>
-                </>
-            ) : (
-                <mesh position={[10, 2.5, 0]}>
-                    <boxGeometry args={[0.2, 5, 20]} />
-                    <meshStandardMaterial color={color} />
-                </mesh>
-            )}
-        </group>
-    )
-}
-
-function DoorHandler({ controlsRef }) {
-    const { camera } = useThree()
-
-    return (
-        <ClickableDoorZone
-            position={[-10, 2.5, 0]}
-            onClick={() => {
-                console.log("Activation salle 2")
-                camera.position.set(-10, 5, 0)
-                controlsRef.current.target.set(-20, 2.5, 0)
-                controlsRef.current.update()
-            }}
-        />
-    )
-}
-
 export default function Museum3D() {
     const floorRef = useRef()
-    const secondFloorRef = useRef()
+    const { roomId } = useParams()
     const controlsRef = useRef()
+    const [activeRoom, setActiveRoom] = useState('center')
+
+    const renderRoom = () => {
+        switch (roomId) {
+            case 'room1': return <RoomStyle1 controlsRef={controlsRef} />
+            case 'room2': return <RoomStyle2 controlsRef={controlsRef} />
+            case 'room3': return <RoomStyle3 controlsRef={controlsRef} />
+            case 'room4': return <RoomStyle4 controlsRef={controlsRef} />
+            case 'trending': default: return <RoomTD controlsRef={controlsRef} />
+        }
+    }
 
     return (
-        <div style={{ height: '100vh', width: '100%' }}>
+        <div className='relative w-full h-screen'>
+            <MuseumMenu />
             <Canvas shadows camera={{ position: [0, 5, 15], fov: 60 }}>
                 <ambientLight intensity={0.3} />
-                <spotLight position={[5, 10, 5]} angle={0.4} penumbra={0.2} intensity={1.5} castShadow />
+                <spotLight position={[5, 10, 5]} angle={0.5} penumbra={0.2} intensity={1.5} castShadow />
 
-                {/* Salles alignées et connectées */}
-                <Room position={[-20, 0, 0]} color="#fce4ec" withRightDoor floorRef={secondFloorRef} />
-                <DoorHandler controlsRef={controlsRef} />
+                {renderRoom()}
 
-                <Room position={[0, 0, 0]} color="#ffffff" withLeftDoor withRightDoor floorRef={floorRef} />
-                <Room position={[20, 0, 0]} color="#e3f2fd" withLeftDoor />
-
-                <ClickToMove floorRef={floorRef} controlsRef={controlsRef} />
-                <ClickToMove floorRef={secondFloorRef} controlsRef={controlsRef} />
+                <ClickToMove floorRef={floorRef} controlsRef={controlsRef} isActive={activeRoom === 'center'} />
 
                 <Controls controlsRef={controlsRef} />
             </Canvas>
