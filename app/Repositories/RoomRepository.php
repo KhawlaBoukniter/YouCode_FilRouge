6df14faf67fa8af00b97e1f1a5caa90c84eb9c70
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-26 18:43:00 +0000

    feat: create RoomRepository with create method

--- app/Repositories/RoomRepository.php
+++ app/Repositories/RoomRepository.php
@@ -2,17 +2,12 @@
 
 namespace App\Repositories;
 
-use App\Models\Artist;
 use App\Models\Room;
 
 class RoomRepository
 {
-    public function createForArtist(Artist $artist, string $style = 'room1'): Room
+    public function create($data): Room
     {
-        return Room::create([
-            'artist_id' => $artist->id,
-            'name' => $artist->user->name,
-            'style' => $style,
-        ]);
+        return Room::create($data);
     }
 }
