ee5ef3cd1a58e2c44109d95138dc1e1c547cc306
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-27 16:23:00 +0000

    feat: add routes for Login and Register pages using AuthLayout

--- artspace-frontend/src/App.js
+++ artspace-frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Museum3D from './pages/3D/Museum3D';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/museum/:roomId" element={<Museum3D />} />
       <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   );
 }
