29f9e17f2069a8659077ff708cac9a8bcae8aa03
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-03 14:47:00 +0000

    feat: add HeroSection for Gallery page with intro text

---
+++ artspace-frontend/src/components/gallery/HeroSection.js
@@ -0,0 +1,19 @@
+import { SearchIcon } from "lucide-react";
+import React from "react";
+
+const HeroSection = () => {
+
+    return (
+        <div className="mx-auto text-center mt-14">
+            <h1 className="font-playfair font-normal text-black text-7xl leading-loose mb-8">
+                Art Gallery
+            </h1>
+            <p className="mx-auto w-10/12 font-playfair font-normal text-gray-600 text-lg leading-7">
+                Discover our extensive collection of masterpieces spanning centuries
+                of artistic excellence.
+            </p>
+        </div>
+    );
+};
+
+export default HeroSection;
