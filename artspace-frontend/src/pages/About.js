89f49894d82f2ba1f3688130ccb641869a0234ac
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-03 11:19:00 +0000

    feat: add About page with all visual sections integrated

---
+++ artspace-frontend/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Section from "../components/about/Section";
+import MissionSection from "../components/about/MissionSection";
+import ExperienceSection from "../components/about/ExperienceSection";
+import CuratorsSection from "../components/about/CuratorsSection";
+import Footer from "../components/Footer";
+
+export default function About() {
+    return (
+        <main className="flex flex-col w-full">
+            <Navbar />
+            <Section />
+            <MissionSection />
+            <ExperienceSection />
+            <CuratorsSection />
+            <Footer />
+        </main>
+    );
+}
