74eadead846a6c814cdd8719c8de08ccb70ae635
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-05 13:42:00 +0000

    feat: create UserDash page with profile structure

---
+++ artspace-frontend/src/pages/UserDash.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Profile from "../components/userDash/Profile";
+import Stats from "../components/userDash/Stats";
+import FavoriteArtworks from "../components/userDash/FavoriteArtworks";
+import PurchaseHistory from "../components/userDash/PurchaseHistory";
+
+export default function UserDash() {
+    return (
+        <div className="flex flex-col min-h-screen bg-[#f8f7f4]">
+            <Navbar />
+            <main className="flex-1 w-full py-24 px-5 md:px-20">
+                <div className="max-w-[1280px] mx-auto space-y-8">
+                    <Profile />
+                    {/* <Stats />
+                    <FavoriteArtworks />
+                    <PurchaseHistory /> */}
+                </div>
+            </main>
+            <Footer />
+        </div>
+    );
+}
