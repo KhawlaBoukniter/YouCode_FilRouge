2e126ad06ff7b96cccb8499d87a287db542e3b02
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-05 15:03:00 +0000

    feat: remove buttom border for card title

--- artspace-frontend/src/components/ui/card.js
+++ artspace-frontend/src/components/ui/card.js
@@ -14,7 +14,7 @@ export function CardContent({ children, className = "" }) {
 
 export function CardHeader({ children, className = "" }) {
     return (
-        <div className={`p-6 border-b border-gray-200 ${className}`}>
+        <div className={`p-6 ${className}`}>
             {children}
         </div>
     );
