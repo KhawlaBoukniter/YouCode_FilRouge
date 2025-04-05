a10824640eaa7be2a656527dc94b5554691d9ef2
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-05 19:10:00 +0000

    feat: create ArtistDash page linking all artist components

---
+++ artspace-frontend/src/pages/ArtistDash.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Header from "../components/artistDash/Header";
+import Stats from "../components/artistDash/Stats";
+import RecentArtworks from "../components/artistDash/RecentArtworks";
+import UpcomingEvents from "../components/artistDash/UpcomingEvents";
+
+export default function ArtistDash() {
+    return (
+        <div className="flex flex-col min-h-screen bg-[#f8f7f4]">
+            <Navbar />
+            <main className="flex-1 w-full py-24 px-5 md:px-20">
+                <div className="max-w-[1280px] mx-auto space-y-12">
+                    <Header />
+                    <Stats />
+                    <RecentArtworks />
+                    <UpcomingEvents />
+                </div>
+            </main>
+            <Footer />
+        </div>
+    );
+}
